c64b5e84b2cd79dc43cb64a1c14f588571f9f7d4
satyjeetsingh1 <am.satyajeet@gmai.com>
2022-02-13 02:17:18 +0530

    second

---
+++ Tic-Tac/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import TicTacToe from './TicTacToe/TicTacToe';
+
+
+
+
+function App() {
+	return (
+		<div className='App'>
+			<TicTacToe />
+		</div>
+	);
+}
+
+export default App;
